a40151c7c560cad87b0518f44168a5d670f9a923
Paul Sealock <psealock@gmail.com>
2018-10-18 17:37:28 +1300

    Persist date related parameters on report navigation

--- client/layout/controller.js
+++ client/layout/controller.js
@@ -4,15 +4,16 @@
  */
 import { Component, createElement } from '@wordpress/element';
 import { parse } from 'qs';
-import { find } from 'lodash';
+import { find, omitBy, isUndefined, last } from 'lodash';
 
 /**
- * External dependencies
+ * Internal dependencies
  */
 import Analytics from 'analytics';
 import AnalyticsReport from 'analytics/report';
 import Dashboard from 'dashboard';
 import DevDocs from 'devdocs';
+import { stringifyQuery } from 'lib/nav-utils';
 
 const getPages = () => {
 	const pages = [
@@ -53,10 +54,25 @@ class Controller extends Component {
 		const search = this.props.location.search.substring( 1 );
 		const query = parse( search );
 		const page = find( getPages(), { path } );
+		window.wpNavMenuUrlUpdate( query );
 		window.wpNavMenuClassChange( page.wpMenu, this.props.location.pathname );
 		return createElement( page.container, { params, path: url, pathMatch: path, query } );
 	}
 }
+
+window.wpNavMenuUrlUpdate = function( { period, compare, after, before } ) {
+	const timeRelatedQuery = omitBy( { period, compare, after, before }, isUndefined );
+	const search = stringifyQuery( timeRelatedQuery );
+
+	Array.from( document.querySelectorAll( '#toplevel_page_wc-admin--analytics a' ) ).forEach(
+		function( item ) {
+			const hashUrl = last( item.href.split( 'wc-admin#' ) );
+			const base = hashUrl.split( '?' )[ 0 ];
+			const href = '/wp-admin/admin.php?page=wc-admin#' + base + search;
+			item.href = href;
+		}
+	);
+};
 
 // When the route changes, we need to update wp-admin's menu with the correct section & current link
 window.wpNavMenuClassChange = function( menuClass, pathname ) {
